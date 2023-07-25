7ef7f2a91a89070600e33a00144d8881efd40cf4
Cameron Fung <Cameronfuung@gmail.com>
2023-07-24 23:55:31 -0700

    Added side bar and home page nav bar

---
+++ src/components/ui/navigation/PermanentDrawerLeft.jsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Logo from '../../../assets/logo-color-cropped.png'
+
+
+const drawerWidth = 240;
+
+export default function PermanentDrawerLeft(props) {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <img src={Logo} alt="Logo" />
+        <Divider />
+        <List>
+          {['Community', 'How To Play', 'Type Chart', 'Bug Reporting', "Sign Out"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        {props.children}
+      </Box>
+    </Box>
+  );
+}
