8f09fe002512d52176a506883670da2f67c24da5
Cameron Fung <Cameronfuung@gmail.com>
2023-08-24 23:56:12 -0700

    added otehr tips

--- src/components/TypeToolTip.jsx
+++ src/components/TypeToolTip.jsx
@@ -17,128 +17,199 @@ const TypeToolTip = ({ displayedType, children, delay }) => {
         "normal": {
 
             colour: "#aa9",
-            none: ["ghost"],
-            not: ["rock", "steel"],
-            super: [],
+            noEffectAttacker: ["ghost"],
+            notEffectiveAttacker: ["rock", "steel"],
+            superEffectiveAttacker: [],
+            superEffectiveDefender: [],
+            notVeryEffectiveDefender: [],
+            noEffectDefender: ["ghost"],
         },
         "fire": {
 
             colour: "#f42",
-            none: [],
-            not: ["fire", "water", "rock", "dragon"],
-            super: ["grass", "ice", "bug", "steel"],
+            noEffectAttacker: [],
+            notEffectiveAttacker: ["fire", "water", "rock", "dragon"],
+            superEffectiveAttacker: ["grass", "ice", "bug", "steel"],
+            superEffectiveDefender: ["water", "ground", "rock"],
+            notVeryEffectiveDefender: ["fire", "grass", "ice", "bug", "steel", "fairy"],
+            noEffectDefender: [],
         },
         "water": {
 
             colour: "#39f",
-            none: [],
-            not: ["water", "grass", "dragon"],
-            super: ["fire", "ground", "rock"],
+            noEffectAttacker: [],
+            notEffectiveAttacker: ["water", "grass", "dragon"],
+            superEffectiveAttacker: ["fire", "ground", "rock"],
+
+            superEffectiveDefender: ["grass", "electric"],
+            notVeryEffectiveDefender: ["water", "ice", "steel", "fire",],
+            noEffectDefender: [],
         },
         "electric": {
 
             colour: "#fc3",
-            none: ["ground"],
-            not: ["electric", "grass", "dragon"],
-            super: ["water", "flying"],
+            noEffectAttacker: ["ground"],
+            notEffectiveAttacker: ["electric", "grass", "dragon"],
+            superEffectiveAttacker: ["water", "flying"],
+
+            superEffectiveDefender: ["ground"],
+            notVeryEffectiveDefender: ["electric", "flying", "steel"],
+            noEffectDefender: [],
         },
         "grass": {
 
             colour: "#7c5",
-            none: [],
-            not: ["fire", "grass", "poison", "flying", "bug", "dragon", "steel"],
-            super: ["water", "ground", "rock"],
+            noEffectAttacker: [],
+            notEffectiveAttacker: ["fire", "grass", "poison", "flying", "bug", "dragon", "steel"],
+            superEffectiveAttacker: ["water", "ground", "rock"],
+
+            superEffectiveDefender: ["fire", "ice", "poison", "flying", "bug"],
+            notVeryEffectiveDefender: ["water", "electric", "grass", "ground"],
+            noEffectDefender: [],
         },
         "ice": {
 
             colour: "#6cf",
-            none: [],
-            not: ["fire", "water", "ice", "steel"],
-            super: ["grass", "ground", "flying", "dragon"],
+            noEffectAttacker: [],
+            notEffectiveAttacker: ["fire", "water", "ice", "steel"],
+            superEffectiveAttacker: ["grass", "ground", "flying", "dragon"],
+
+
+            superEffectiveDefender: ["fire", "fighting", "rock", "steel"],
+            notVeryEffectiveDefender: ["ice"],
+            noEffectDefender: [],
         },
         "fighting": {
 
             colour: "#b54",
-            none: ["ghost"],
-            not: ["poison", "flying", "psychic", "bug", "fairy"],
-            super: ["normal", "ice", "rock", "dark", "steel"],
+            noEffectAttacker: ["ghost"],
+            notEffectiveAttacker: ["poison", "flying", "psychic", "bug", "fairy"],
+            superEffectiveAttacker: ["normal", "ice", "rock", "dark", "steel"],
+
+            superEffectiveDefender: ["flying", "psychic", "fairy"],
+            notVeryEffectiveDefender: ["bug", "rock", "dark"],
+            noEffectDefender: [],
         },
         "poison": {
 
             colour: "#a59",
-            none: ["steel"],
-            not: ["poison", "ground", "rock", "ghost"],
-            super: ["grass", "fairy"],
+            noEffectAttacker: ["steel"],
+            notEffectiveAttacker: ["poison", "ground", "rock", "ghost"],
+            superEffectiveAttacker: ["grass", "fairy"],
+
+            superEffectiveDefender: ["ground", "psychic"],
+            notVeryEffectiveDefender: ["grass", "fighting", "poison", "bug", "fairy"],
+            noEffectDefender: [],
         },
         "ground": {
 
             colour: "#db5",
-            none: ["flying"],
-            not: ["grass", "bug"],
-            super: ["fire", "electric", "poison", "rock", "steel"],
+            noEffectAttacker: ["flying"],
+            notEffectiveAttacker: ["grass", "bug"],
+            superEffectiveAttacker: ["fire", "electric", "poison", "rock", "steel"],
+
+            superEffectiveDefender: ["water", "grass", "ice"],
+            notVeryEffectiveDefender: ["poison", "rock"],
+            noEffectDefender: ["electric"],
         },
         "flying": {
 
             colour: "#89f",
-            none: [],
-            not: ["electric", "rock", "steel"],
-            super: ["grass", "fighting", "bug"],
+            noEffectAttacker: [],
+            notEffectiveAttacker: ["electric", "rock", "steel"],
+            superEffectiveAttacker: ["grass", "fighting", "bug"],
+
+            superEffectiveDefender: ["electric", "ice", "rock"],
+            notVeryEffectiveDefender: ["grass", "fighting", "bug"],
+            noEffectDefender: ["ground"],
         },
         "psychic": {
 
             colour: "#f59",
-            none: ["dark"],
-            not: ["psychic", "steel"],
-            super: ["fighting", "poison"],
+            noEffectAttacker: ["dark"],
+            notEffectiveAttacker: ["psychic", "steel"],
+            superEffectiveAttacker: ["fighting", "poison"],
+
+            superEffectiveDefender: ["bug", "ghost", "dark"],
+            notVeryEffectiveDefender: ["fighting", "psychic"],
+            noEffectDefender: [],
         },
         "bug": {
 
             colour: "#ab2",
-            none: [],
-            not: ["fire", "fighting", "poison", "flying", "ghost", "steel", "fairy"],
-            super: ["grass", "psychic", "dark"],
+            noEffectAttacker: [],
+            notEffectiveAttacker: ["fire", "fighting", "poison", "flying", "ghost", "steel", "fairy"],
+            superEffectiveAttacker: ["grass", "psychic", "dark"],
+
+            superEffectiveDefender: ["fire", "flying", "rock"],
+            notVeryEffectiveDefender: ["grass", "fighting", "ground"],
+            noEffectDefender: [],
         },
         "rock": {
 
             colour: "#ba6",
-            none: [],
-            not: ["fighting", "ground", "steel"],
-            super: ["fire", "ice", "flying", "bug"],
+            noEffectAttacker: [],
+            notEffectiveAttacker: ["fighting", "ground", "steel"],
+            superEffectiveAttacker: ["fire", "ice", "flying", "bug"],
+
+            superEffectiveDefender: ["fighting", "ground", "steel", "water", "grass"],
+            notVeryEffectiveDefender: ["normal", "fire", "poison", "flying"],
+            noEffectDefender: [],
         },
         "ghost": {
 
             colour: "#66b",
-            none: ["normal"],
-            not: ["dark"],
-            super: ["psychic", "ghost"],
+            noEffectAttacker: ["normal"],
+            notEffectiveAttacker: ["dark"],
+            superEffectiveAttacker: ["psychic", "ghost"],
+
+            superEffectiveDefender: ["ghost", "dark"],
+            notVeryEffectiveDefender: ["poison", "bug"],
+            noEffectDefender: ["normal", "fighting"],
         },
         "dragon": {
 
             colour: "#76e",
-            none: ["fairy"],
-            not: ["steel"],
-            super: ["dragon"],
+            noEffectAttacker: ["fairy"],
+            notEffectiveAttacker: ["steel"],
+            superEffectiveAttacker: ["dragon"],
+
+            superEffectiveDefender: ["ice", "dragon", "fairy"],
+            notVeryEffectiveDefender: ["fire", "water", "electric", "grass"],
+            noEffectDefender: [],
         },
         "dark": {
 
             colour: "#754",
-            none: [],
-            not: ["fighting", "dark", "fairy"],
-            super: ["psychic", "ghost"],
+            noEffectAttacker: [],
+            notEffectiveAttacker: ["fighting", "dark", "fairy"],
+            superEffectiveAttacker: ["psychic", "ghost"],
+
+            superEffectiveDefender: ["fighting", "bug", "fairy"],
+            notVeryEffectiveDefender: ["ghost", "dark"],
+            noEffectDefender: ["psychic"],
         },
         "steel": {
 
             colour: "#aab",
-            none: [],
-            not: ["fire", "water", "electric", "steel"],
-            super: ["ice", "rock", "fairy"],
+            noEffectAttacker: [],
+            notEffectiveAttacker: ["fire", "water", "electric", "steel"],
+            superEffectiveAttacker: ["ice", "rock", "fairy"],
+
+            superEffectiveDefender: ["fire", "fighting", "ground"],
+            notVeryEffectiveDefender: ["normal", "grass", "ice", "flying", "psychic", "bug", "rock", "dragon", "steel", "fairy"],
+            noEffectDefender: ["poison"],
         },
         "fairy": {
 
             colour: "#e9e",
-            none: [],
-            not: ["fire", "poison", "steel"],
-            super: ["fighting", "dragon", "dark"],
+            noEffectAttacker: [],
+            notEffectiveAttacker: ["fire", "poison", "steel"],
+            superEffectiveAttacker: ["fighting", "dragon", "dark"],
+
+            superEffectiveDefender: ["poison", "steel"],
+            notVeryEffectiveDefender: ["fighting", "bug", "dark"],
+            noEffectDefender: ["dragon"],
         },
     };
 
@@ -148,13 +219,14 @@ const TypeToolTip = ({ displayedType, children, delay }) => {
             enterDelay={delay}
             enterNextDelay={delay}
             arrow
+            open={true}
             title={
-                <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
-                    <Paper elevation={3} style={{ padding: "16px" }}>
+                <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+                    <Paper elevation={3} style={{ padding: "5px" }}>
                         <div>
                             <h3 style={{ marginBottom: "8px" }}>Super Effective</h3>
                             <div>
-                                {pokemonTypes[displayedType]?.super.map((type, i) => (
+                                {pokemonTypes[displayedType]?.superEffectiveAttacker.map((type, i) => (
                                     <span class={capitalizeFirstLetter(type)} key={i} style={typeLabelStyles}>
                                         {type}
                                     </span>
@@ -162,11 +234,11 @@ const TypeToolTip = ({ displayedType, children, delay }) => {
                             </div>
                         </div>
                     </Paper>
-                    <Paper elevation={3} style={{ padding: "16px" }}>
+                    <Paper elevation={3} style={{ padding: "5" }}>
                         <div>
                             <h3 style={{ marginBottom: "8px" }}>No Effect</h3>
                             <div>
-                                {pokemonTypes[displayedType]?.none.map((type, i) => (
+                                {pokemonTypes[displayedType]?.noEffectAttacker.map((type, i) => (
                                     <span class={capitalizeFirstLetter(type)} key={i} style={typeLabelStyles}>
                                         {type}
                                     </span>
@@ -174,11 +246,47 @@ const TypeToolTip = ({ displayedType, children, delay }) => {
                             </div>
                         </div>
                     </Paper>
-                    <Paper elevation={3} style={{ padding: "16px" }}>
+                    <Paper elevation={3} style={{ padding: "5" }}>
                         <div>
                             <h3 style={{ marginBottom: "8px" }}>Not Very Effective</h3>
                             <div>
-                                {pokemonTypes[displayedType]?.not.map((type, i) => (
+                                {pokemonTypes[displayedType]?.notEffectiveAttacker.map((type, i) => (
+                                    <span class={capitalizeFirstLetter(type)} key={i} style={typeLabelStyles}>
+                                        {type}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    </Paper>
+                    <Paper elevation={3} style={{ padding: "5" }}>
+                        <div>
+                            <h3 style={{ marginBottom: "8px" }}>Super Effective Against this type</h3>
+                            <div>
+                                {pokemonTypes[displayedType]?.superEffectiveDefender.map((type, i) => (
+                                    <span class={capitalizeFirstLetter(type)} key={i} style={typeLabelStyles}>
+                                        {type}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    </Paper>
+                    <Paper elevation={3} style={{ padding: "5" }}>
+                        <div>
+                            <h3 style={{ marginBottom: "8px" }}>Not Very Effective against this type</h3>
+                            <div>
+                                {pokemonTypes[displayedType]?.notVeryEffectiveDefender.map((type, i) => (
+                                    <span class={capitalizeFirstLetter(type)} key={i} style={typeLabelStyles}>
+                                        {type}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+                    </Paper>
+                    <Paper elevation={3} style={{ padding: "5" }}>
+                        <div>
+                            <h3 style={{ marginBottom: "8px" }}>No Effect Against this type</h3>
+                            <div>
+                                {pokemonTypes[displayedType]?.noEffectDefender.map((type, i) => (
                                     <span class={capitalizeFirstLetter(type)} key={i} style={typeLabelStyles}>
                                         {type}
                                     </span>
